173d56761b6e1b9ce672a89cdb328d1c65acd288
Bea Cavalheiro <biacavalheiro2bia@gmail.com>
2022-11-05 06:01:25 -0300

    .

--- Backend/src/main/java/br/com/org/recode/repositorios/CargoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package br.com.org.recode.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.recode.projetos.entidades.Cargo;
-
-public interface CargoRepositorio extends JpaRepository<Cargo, Long> {
-    
-}
